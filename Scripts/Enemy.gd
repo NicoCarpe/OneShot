d499fc1df2ad45e511fd4941098605ee2602e2f6
Struckdown <bfleysher@gmail.com>
2018-11-11 11:35:54 -0700

    Raycasting means enemies can't see you

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -8,15 +8,25 @@ func _ready():
 func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
-#	pass
-	rotation += get_angle_to(player.position)
-	var movedir = Vector2(1,0).rotated(rotation)
-	var motion = movedir.normalized() * MOTION_SPEED
-	var collision = move_and_collide(motion*delta)
-	if collision:
-		if collision.collider.is_in_group("Player"):
-			collision.collider.playerHit()
-		move_and_slide(motion)
+	if canSeeTarget():
+		rotation += get_angle_to(player.position)
+		var movedir = Vector2(1,0).rotated(rotation)
+		var motion = movedir.normalized() * MOTION_SPEED
+		var collision = move_and_collide(motion*delta)
+		if collision:
+			if collision.collider.is_in_group("Player"):
+				collision.collider.playerHit()
+			move_and_slide(motion)
+
+func canSeeTarget():
+	#Raycast to check if can see target
+	var space_state = get_world_2d().direct_space_state
+	var result = space_state.intersect_ray(position, player.position, [self], 3) # Hits environment, player
+	if result:
+		if result.collider.is_in_group("Player"): # Sees player and nothing inbetween
+			return true
+	return false # Failed to detect target
+
 
 func onHit():
 	$AudioStreamPlayer2D.play()
