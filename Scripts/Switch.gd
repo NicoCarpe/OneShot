01d45fd073de67487c19b2021c6875ec78fd3034
Struckdown <bfleysher@gmail.com>
2018-11-12 08:57:41 -0700

    Updated switch graphic

--- Scripts/Switch.gd
+++ Scripts/Switch.gd
@@ -3,6 +3,7 @@ extends Area2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var x = 0
 
 signal onHit
 
@@ -11,12 +12,13 @@ func _ready():
 	# Initialization here
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	$Sprite.position.y = sin(x/20)*2
+	x += 1
+
 
 func _on_Switch_body_entered(body):
 	if body.is_in_group("Bullet"):
 		emit_signal("onHit")
 		$AudioStreamPlayer2D.play()
+		$Sprite.texture = load("res://Tiles/switch of.png")
